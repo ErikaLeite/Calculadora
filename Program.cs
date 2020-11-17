3e83a977155c678886722e9325df3d1b638b4e4f
ERIKA LEITE PAIVA <you@example.com>
2020-11-17 10:47:34 -0300

    Estrutura montada, erro precisa ser corrigido

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculadora
+            //Escreva um algoritmo que receba o número inteiro e exiba sua tabuada até 10
+            Console.WriteLine("TABUADA"); 
+            Console.WriteLine("Digite um número inteiro:");
+            int numero = int.Parse(Console.ReadLine());
+
+            int formula, contador;
+            for (contador = 1; contador <= 10; ++contador)
+            {
+                formula =  numero * contador;
+                Console.WriteLine(numero + " X " + contador + " = " + formula);
+
+                //erro prescisa ser resolvido
+
+        }
+    }
+}

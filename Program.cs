22346413c661e06ec8c1503f980e7f5fc74c1fbb
ERIKA LEITE PAIVA <you@example.com>
2020-11-18 09:47:08 -0300

    Erro corrigido

--- Program.cs
+++ Program.cs
@@ -9,17 +9,18 @@ namespace Calculadora
             //Calculadora
             //Escreva um algoritmo que receba o número inteiro e exiba sua tabuada até 10
             Console.WriteLine("TABUADA"); 
+            Console.WriteLine("  "); 
             Console.WriteLine("Digite um número inteiro:");
             int numero = int.Parse(Console.ReadLine());
 
-            int formula, contador;
-            for (contador = 1; contador <= 10; ++contador)
+            int resultado, contador;
+            for (contador = 0; contador <= 10; ++contador)
             {
-                formula =  numero * contador;
-                Console.WriteLine(numero + " X " + contador + " = " + formula);
-
-                //erro prescisa ser resolvido
-
+                resultado =  numero * contador;
+                Console.WriteLine(numero+ " X " +contador + " = " +resultado);
+                //contatenação é juntar elementos do código
+                //interpolação 
+            }
         }
     }
 }
